01b44f384ebbac887893c9b52b5aca2098aec9e0
Thesara Subasinghe <88161607+thesarakasun@users.noreply.github.com>
2022-10-15 21:08:52 +0530

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!-- Coding by CodingLab | www.codinglabweb.com-->
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!--<title> Circular Progress Bar </title>-->
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="style.css">
+                                        
+    </head>
+    <body>
+        <div class="container">
+            <div class="circular-progress">
+                <span class="progress-value">50%</span>
+            </div>
+
+            <b><span class="text">Battery Percentage</span></b>
+            <span >Charging power: 483.0W</span>
+        </div>
+
+        <!-- JavaScript -->
+        <script src="script.js"></script>
+    </body>
+</html>
---
+++ home2.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1,user-scalable=no" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <title>Impressing</title>
+</head>
+
+
+<body>
+
+    <!-- our services desktop part starts here -->
+    <!-- section 3 -->
+    <div class="section-3" id="oursdesk">
+        <center>
+
+            <h1 class="pt-4">Our Products</h1>
+        </center>
+
+        <div class="container pt-4 ">
+            <div class="row mt-4 g-3 justify-content-center">
+                <div data-aos="fade-up" data-aos-delay="50" data-aos-duration="1500" class="col-md-4">
+                        <div class="container ">
+            <div class="circular-progress">
+                <span class="progress-value">50%</span>
+            </div>
+
+            <b><span class="text">Battery Percentage</span></b>
+            <span >Charging power: 483.0W</span>
+        </div>
+                </div>
+
+
+
+           
+
+
+
+                <div data-aos="fade-up" data-aos-delay="50" data-aos-duration="1500" class="col-md-4">
+                    <div class="card" style="width: 20rem;">
+                        <img src="del6.jpg" height="300px" class="card-img-top" alt="...">
+                        <div class="card-body bg-white text-dark">
+                            <h5 class="card-title">Product 02</h5>
+                            <p class="card-text">Product code : SAC0002</p>
+                            <a href="https://wa.me/+94725842428?text=SAC0002" class="btn  btn-primary ">Order via WhatsApp</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div data-aos="fade-up" data-aos-delay="50" data-aos-duration="1500" class="col-md-4">
+                    <div class="card" style="width: 20rem;">
+                        <img src="pics5.jpg" height="300px" class="card-img-top" alt="...">
+                        <div class="card-body bg-white text-dark">
+                            <h5 class="card-title">Product 03</h5>
+                            <p class="card-text">Product code : SAC0003</p>
+                            <a href="https://wa.me/+94725842428?text=SAC0003" class="btn btn-primary  ">Order via WhatsApp</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container pt-3 ">
+            <div class="row mt-4 g-3 justify-content-center">
+                <div data-aos="fade-up" data-aos-delay="50" data-aos-duration="1500" class="col-md-4">
+                    <div class="card" style="width: 20rem;">
+                        <img src="pics2.jpg" height="300px" class="card-img-top" alt="...">
+                        <div class="card-body bg-white text-dark">
+                            <h5 class="card-title"> product 04</h5>
+                            <p class="card-text">Product code : SAC0004</p>
+                            <a href="https://wa.me/+94725842428?text=SAC0004" class="btn btn-primary ">Order via WhatsApp</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div data-aos="fade-up" data-aos-delay="50" data-aos-duration="1500" class="col-md-4">
+                    <div class="card" style="width: 20rem;">
+                        <img src="pics3.jpg" height="300px" class="card-img-top" alt="...">
+                        <div class="card-body bg-white text-dark">
+                            <h5 class="card-title">Product 05</h5>
+                            <p class="card-text">Product code : SAC0005</p>
+                            <a href="https://wa.me/+94725842428?text=SAC0005" class="btn  btn-primary">Order via WhatsApp</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div data-aos="fade-up" data-aos-delay="50" data-aos-duration="1500" class="col-md-4">
+                    <div class="card" style="width: 20rem;">
+                        <img src="del4.png" height="300px" class="card-img-top" alt="...">
+                        <div class="card-body bg-white text-dark">
+                            <h5 class="card-title">Product 06</h5>
+                            <p class="card-text">Product code : SAC0006</p>
+                            <a href="https://wa.me/+94725842428?text=SAC0006" class="btn btn-primary  "> Order via WhatsApp</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="container pt-3 ">
+            <div class="row mt-4 g-3 justify-content-center">
+                <div data-aos="fade-up" data-aos-delay="50" data-aos-duration="1500" class="col-md-4">
+                    <div class="card" style="width: 20rem;">
+                        <img src="pics2.jpg" height="300px" class="card-img-top" alt="...">
+                        <div class="card-body bg-white text-dark">
+                            <h5 class="card-title"> product 07</h5>
+                            <p class="card-text">Product code : SAC0004</p>
+                            <a href="https://wa.me/+94725842428?text=SAC0007" class="btn btn-primary ">Order via WhatsApp</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div data-aos="fade-up" data-aos-delay="50" data-aos-duration="1500" class="col-md-4">
+                    <div class="card" style="width: 20rem;">
+                        <img src="pics3.jpg" height="300px" class="card-img-top" alt="...">
+                        <div class="card-body bg-white text-dark">
+                            <h5 class="card-title">Product 08</h5>
+                            <p class="card-text">Product code : SAC0005</p>
+                            <a href="https://wa.me/+94725842428?text=SAC0008" class="btn  btn-primary">Order via WhatsApp</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div data-aos="fade-up" data-aos-delay="50" data-aos-duration="1500" class="col-md-4">
+                    <div class="card" style="width: 20rem;">
+                        <img src="del4.png" height="300px" class="card-img-top" alt="...">
+                        <div class="card-body bg-white text-dark">
+                            <h5 class="card-title">Product 09</h5>
+                            <p class="card-text">Product code : SAC0006</p>
+                            <a href="https://wa.me/+94725842428?text=SAC0009" class="btn btn-primary  "> Order via WhatsApp</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
+
+</body>
+
+</html>
